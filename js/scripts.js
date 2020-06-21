855972833ccea7ebcd4f3b33d96996db67345840
Anna <annalez16@gmail.com>
2020-06-21 23:22:42 +0300

    Add f-n clearResult page styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 const userInputElement = document.getElementById("input");
 const button = document.getElementById("button");
-let spinner = document.getElementById("spinner")
+const spinner = document.getElementById("spinner");
+list = document.getElementById("list");
 const apiKey = "e2d2b008fa8386faff19423eff47af86";
 
-button.addEventListener("click", () => {  
-  getCompany();
+button.addEventListener("click", () => {
+  getSearchResult();
 });
 
-async function getCompany() {
+function clearResult() {
+  document.getElementById("list").innerHTML = " ";
+}
+
+async function getSearchResult() {
+  clearResult();
   let userSearch = userInputElement.value;
   toggleSpinner();
   let response = await fetch(
@@ -15,14 +21,21 @@ async function getCompany() {
   );
   let data = await response.json();
   toggleSpinner();
-  console.log(data);
-  return data;
+  presentCompList(data);
 }
 
-function presentCompList () {
-
+function presentCompList(array) { 
+  let li = document.createElement("li");
+  li.classList.add("list-group-item", "pl-0", "pr-0");
+  let getresults = "";
+  for (company of array) {
+    getresults += `<a href class = "list-group-item">${company.symbol} 
+      ${company.name}</a>`;
+  }
+  li.innerHTML = getresults;
+  list.appendChild(li);
 }
 
-function toggleSpinner () {
-    spinner.classList.toggle("d-none");
+function toggleSpinner() {
+  spinner.classList.toggle("d-none");
 }
