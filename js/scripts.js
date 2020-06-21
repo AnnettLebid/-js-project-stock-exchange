5d71dcbe2cf63b68da7af6605a9d093f65fd7923
Anna <annalez16@gmail.com>
2020-06-21 17:17:29 +0300

    Add function toggleSpinner

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const userInputElement = document.getElementById("input");
 const button = document.getElementById("button");
-let userSearch = userInputElement.value;
+let spinner = document.getElementById("spinner")
 const apiKey = "e2d2b008fa8386faff19423eff47af86";
 
 button.addEventListener("click", () => {  
@@ -9,10 +9,20 @@ button.addEventListener("click", () => {
 
 async function getCompany() {
   let userSearch = userInputElement.value;
+  toggleSpinner();
   let response = await fetch(
     `https://financialmodelingprep.com/api/v3/search?query=${userSearch}&limit=10&exchange=NASDAQ&apikey=${apiKey}`
   );
   let data = await response.json();
+  toggleSpinner();
   console.log(data);
   return data;
+}
+
+function presentCompList () {
+
+}
+
+function toggleSpinner () {
+    spinner.classList.toggle("d-none");
 }
