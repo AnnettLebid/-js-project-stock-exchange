72716333da9e085829433aa2345cc7851459fc5e
Anna <annalez16@gmail.com>
2020-06-22 23:41:46 +0300

    Add chart and implement namespacing

--- js/company.js
+++ js/company.js
@@ -1,36 +1,97 @@
-let urlParams = new URLSearchParams(window.location.search);
-let compSymbol = urlParams.get("symbol");
-const apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+(() => {
+  const companyData = {};
+  let companies = {};
+  companies.graph = {};
+  companies.graph.xYears = [];
+  companies.graph.yPrices = [];
 
-async function getCompanyProfile() {
-  let response = await fetch(
-    `https://financialmodelingprep.com/api/v3/profile/${compSymbol}?apikey=${apiKey}`
-  );
-  let data = await response.json();
-  makeCompProfile(data);
-  console.log(data);
-  return data;
-}
-
-function makeCompProfile(data) {
-  let {
-    companyName,
-    sector,
-    price,
-    description,
-    image,
-    changes,
-    website,
-  } = data[0];
+  getStoredElements();
+  getCompanyProfile();
+  getComPriceHistory();
   
-  document.getElementById("logo").src = image;
-  document.getElementById("comp-name").textContent = companyName;
-  document.getElementById("comp-name").setAttribute("href", `${website}`);
-  document.getElementById("sector").textContent = sector;
-  document.getElementById("price").textContent = price;
-  document.getElementById("changes").textContent = changes;
-  document.getElementById("description").textContent = description;
-}
 
-// makeCompProfile();
-getCompanyProfile();
+  function getStoredElements() {
+    companyData.compSymbol = new URLSearchParams(window.location.search).get(
+      "symbol"
+    );
+    companyData.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+  }
+
+  async function getCompanyProfile() {
+    const { compSymbol, apiKey } = companyData;
+    let response = await fetch(
+      `https://financialmodelingprep.com/api/v3/profile/${compSymbol}?apikey=${apiKey}`
+    );
+    let data = await response.json();
+    makeCompProfile(data);
+    return data;
+  }
+
+  function makeCompProfile(data) {
+    let {
+      companyName,
+      sector,
+      price,
+      description,
+      image,
+      changes,
+      website,
+    } = data[0];
+
+    document.getElementById("logo").src = image;
+    document.getElementById("comp-name").textContent = companyName;
+    document.getElementById("comp-name").setAttribute("href", `${website}`);
+    document.getElementById("sector").textContent = sector;
+    document.getElementById("price").textContent = price;
+    document.getElementById("changes").textContent = changes;
+    document.getElementById("description").textContent = description;
+  }
+
+  async function getComPriceHistory() {
+    const { compSymbol, apiKey } = companyData;
+    let response = await fetch(
+      `https://financialmodelingprep.com/api/v3/historical-price-full/${compSymbol}?serietype=line&apikey=${apiKey}`
+    );
+    let data = await response.json();
+    let priceHistory = data.historical;
+    for (let i = 0; i < priceHistory.length; i++) {
+      companies.graph.xYears.unshift(priceHistory[i].date);
+      companies.graph.yPrices.unshift(priceHistory[i].close);
+    }
+    let xYears = companies.graph.xYears;
+    let yPrices = companies.graph.yPrices   
+   
+    await makeCompChart(xYears, yPrices);    
+  }
+  
+
+  async function makeCompChart(years, price) {
+    // await getComPriceHistory();
+    let ctx = document.getElementById("myChart").getContext("2d");
+    let chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: years,
+        datasets: [
+          {
+            label: "Stock Price History",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: price,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+  }
+})();
--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,55 @@
-const userInputElement = document.getElementById("input");
-const button = document.getElementById("button");
-const spinner = document.getElementById("spinner");
-let list = document.getElementById("list");
-const apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+(() => {
+  const stockData = {};
+  getElements();
 
-button.addEventListener("click", () => {
-  getCompany();
-});
-
-function clearResult() {
-  document.getElementById("list").innerHTML = " ";
-}
-
-async function getCompany() {
-  clearResult();
-  let userSearch = userInputElement.value;
-  toggleSpinner();
-  let response = await fetch(
-    `https://financialmodelingprep.com/api/v3/search?query=${userSearch}&limit=10&exchange=NASDAQ&apikey=${apiKey}`
-  );
-  let data = await response.json();
-  toggleSpinner();
-  presentCompList(data);
-}
-
-function presentCompList(array) {
-  let li = document.createElement("li");
-  li.classList.add("list-group-item", "pl-0", "pr-0");
-  let getresults = "";
-  for (company of array) {
-    getresults += `<a href = "./company.html?symbol=${company.symbol}" class = "list-group-item">${company.symbol} 
-      ${company.name}</a>`;   
+  function getElements() {
+    stockData.userInputElement = document.getElementById("input");
+    stockData.spinner = document.getElementById("spinner");
+    stockData.list = document.getElementById("list");
+    stockData.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+    stockData.button = document
+      .getElementById("button")
+      .addEventListener("click", () => {
+        getCompany(getUserSearch());
+      });
   }
-  li.innerHTML = getresults;
-  list.appendChild(li);
-}
 
-function toggleSpinner() {
-  spinner.classList.toggle("d-none");
-}
+  function clearResultsList() {
+    document.getElementById("list").innerHTML = " ";
+  }
+
+  function getUserSearch() {
+    const { userInputElement } = stockData;
+    let userSearch = userInputElement.value;
+    return userSearch;
+  }
+
+  async function getCompany(userSearch) {
+    const { apiKey } = stockData;
+    clearResultsList();
+    toggleSpinner();
+    let response = await fetch(
+      `https://financialmodelingprep.com/api/v3/search?query=${userSearch}&limit=10&exchange=NASDAQ&apikey=${apiKey}`
+    );
+    let data = await response.json();
+    toggleSpinner();
+    createCompaniesList(data);
+  }
+
+  function createCompaniesList(array) {
+    let li = document.createElement("li");
+    li.classList.add("list-group-item", "pl-0", "pr-0");  
+    let getresults = "";
+    for (company of array) {
+      getresults += `<a href = "./company.html?symbol=${company.symbol}" class = "list-group-item">${company.symbol} 
+      ${company.name}</a>`;
+    }
+    li.innerHTML = getresults;
+    list.appendChild(li);
+  }
+
+  function toggleSpinner() {
+    const { spinner } = stockData;
+    spinner.classList.toggle("d-none");
+  }
+})();
