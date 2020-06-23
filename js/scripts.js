029e9c326cb994e2256fe175b1a9057d687d15a0
Anna <annalez16@gmail.com>
2020-06-23 14:14:00 +0300

    Add srcond fetch to get detailed company profile

--- js/scripts.js
+++ js/scripts.js
@@ -31,22 +31,46 @@
     let response = await fetch(
       `https://financialmodelingprep.com/api/v3/search?query=${userSearch}&limit=10&exchange=NASDAQ&apikey=${apiKey}`
     );
-    let data = await response.json();   
+    let companyObjects = await response.json();
+    // let symbol =
+    console.log(companyObjects);
     toggleSpinner();
-    createCompaniesList(data);
+    createCompaniesList(companyObjects);
   }
 
-  function createCompaniesList(array) {
-    let li = document.createElement("li");
-    li.classList.add("list-group-item", "pl-0", "pr-0");  
-    let getresults = "";
-    for (company of array) {
-      getresults += `<a href = "./company.html?symbol=${company.symbol}" class = "list-group-item">${company.symbol} 
-      ${company.name}</a>`;
-    }
-    li.innerHTML = getresults;
-    list.appendChild(li);
+  async function createCompaniesList(companyObjects) {
+    const { apiKey } = stockData;
+    const companiesProfiles = companyObjects.map(async (company) => {
+      const response = await fetch(
+        `https://financialmodelingprep.com/api/v3/company/profile/${company.symbol}?apikey=${apiKey}`
+      );
+      return response.json();
+    });
+    const companyProfile = await Promise.all(companiesProfiles);
+    console.log(companyProfile);
   }
+
+  //   let li = document.createElement("li");
+  //   li.classList.add("list-group-item", "pl-0", "pr-0");
+  //   let getresults = "";
+  //   for (company of array) {
+  //     getresults += `<a href = "./company.html?symbol=${company.symbol}" class = "list-group-item">${company.symbol}
+  //     ${company.name}</a>`;
+  //   }
+  //   li.innerHTML = getresults;
+  //   list.appendChild(li);
+
+  // function createCompaniesList(array) {
+  //   let li = document.createElement("li");
+  //   li.classList.add("list-group-item", "pl-0", "pr-0");
+  //   let getresults = "";
+  //   for (company of array) {
+  //     getresults += `<a href = "./company.html?symbol=${company.symbol}" class = "list-group-item">${company.symbol}
+  //     ${company.name}</a>`;
+  //   }
+  //   li.innerHTML = getresults;
+  //   list.appendChild(li);
+  // }
 
   function toggleSpinner() {
     const { spinner } = stockData;
