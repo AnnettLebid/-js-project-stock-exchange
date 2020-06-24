0c7514b864b1cc726593ff32a272debcbdfa764b
Anna <annalez16@gmail.com>
2020-06-24 18:14:06 +0300

    Make Marquee class

--- js/marquee.js
+++ js/marquee.js
@@ -1,43 +1,36 @@
-(() => {
-  const marqueeData = {};
-
-  getElements();
-  loadMarquee();
- 
-  function getElements() {
-    marqueeData.marquee = document.getElementById("marquee");
-    marqueeData.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+class Marquee {
+  constructor(parentElement) {
+    this.parentElement = parentElement;
+    this.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
   }
 
-  async function getStockPriceData() {
-    const { apiKey } = marqueeData;
+  async loadMarquee() {
+    const pricesArray = await this.getStockPriceData();
+    await pricesArray.map((company) => {
+      this.createMarquee(company);
+    });
+  }
+
+  async getStockPriceData() {   
     let response = await fetch(
-      `https://financialmodelingprep.com/api/v3/quotes/nyse?apikey=${apiKey}`
+      `https://financialmodelingprep.com/api/v3/quotes/nyse?apikey=${this.apiKey}`
     );
-    let data = await response.json();    
+    let data = await response.json();
     return data.slice(0, 200);
   }
 
-  async function loadMarquee() {
-    const pricesArray = await getStockPriceData();
-    await pricesArray.map((company) => {
-        createMarquee(company);
-    })
+  createMarquee(company) {   
+    const marqueeContainer = document.createElement("div");
+    marqueeContainer.classList.add("marquee-div", "p-1");
+
+    const divSymbol = document.createElement("div");
+    divSymbol.textContent = `${company.symbol}: `;
+
+    const divPrice = document.createElement("div");
+    divPrice.textContent = `${company.price}$ `;
+    divPrice.classList.add("text-success");
+
+    marqueeContainer.append(divSymbol, divPrice);
+    this.parentElement.appendChild(marqueeContainer);
   }
-
-    function createMarquee(company) {
-        const { marquee } = marqueeData;
-      const marqueeContainer = document.createElement("div");
-      marqueeContainer.classList.add("marquee-div", "p-1");
-
-      const divSymbol = document.createElement("div");    
-      divSymbol.textContent = `${company.symbol}: `;
-
-      const divPrice = document.createElement("div");   
-      divPrice.textContent = `${company.price}$ `;
-      divPrice.classList.add("text-success")
-
-      marqueeContainer.append(divSymbol, divPrice);
-      marquee.appendChild(marqueeContainer);
-    }
-})();
+}
