986da306a4ada00811df2a761fbf5e0c98d67ea6
Anna <annalez16@gmail.com>
2020-06-24 13:35:47 +0300

    Finish milestone: 4 add marquee

--- js/marquee.js
+++ js/marquee.js
@@ -2,9 +2,8 @@
   const marqueeData = {};
 
   getElements();
-  getStockPriceData();
-//   createMarquee(stockPriceArray);
-
+  loadMarquee();
+ 
   function getElements() {
     marqueeData.marquee = document.getElementById("marquee");
     marqueeData.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
@@ -20,5 +19,26 @@
     return data.slice(0, 200);
   }
 
+  async function loadMarquee() {
+    const pricesArray = await getStockPriceData();
+    await pricesArray.map((company) => {
+        createMarquee(company);
+    })
+  }
 
+    function createMarquee(company) {
+        const { marquee } = marqueeData;
+      const marqueeContainer = document.createElement("div");
+      marqueeContainer.classList.add("marquee-div", "p-1");
+
+      const divSymbol = document.createElement("div");    
+      divSymbol.textContent = `${company.symbol}: `;
+
+      const divPrice = document.createElement("div");   
+      divPrice.textContent = `${company.price}$ `;
+      divPrice.classList.add("text-success")
+
+      marqueeContainer.append(divSymbol, divPrice);
+      marquee.appendChild(marqueeContainer);
+    }
 })();
