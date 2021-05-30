f4dcf747626897af881298a2d4d4dfa456266d0a
Anna Lebid <annalez16@gmail.com>
2021-05-30 16:06:37 +0300

    ready for deploy and move back Marque

---
+++ client/js/Marquee.js
@@ -0,0 +1,37 @@
+class Marquee {
+  constructor(parentElement) {
+    this.parentElement = parentElement;    
+  }
+
+  async loadMarquee() {
+    const pricesArray = await this.fetchMarqueeToInternalServer();    
+    await pricesArray.map((company) => {
+      this.createMarquee(company);
+    }); 
+  }
+
+  async fetchMarqueeToInternalServer() {    
+    const response = await fetch(
+      `https://js-stock-exchange.herokuapp.com/stock-price`           
+    );
+    const data = await response.json(); 
+    return data;   
+  }
+ 
+  createMarquee(company) {
+    if (company.price) {
+      const marqueeContainer = document.createElement("div");
+      marqueeContainer.classList.add("marquee-div", "p-1");
+
+      const divSymbol = document.createElement("div");
+      divSymbol.textContent = `${company.symbol}: `;
+
+      const divPrice = document.createElement("div");
+      divPrice.textContent = `${company.price}$ `;
+      divPrice.classList.add("neon-green", "ml-1");
+
+      marqueeContainer.append(divSymbol, divPrice);
+      this.parentElement.appendChild(marqueeContainer);
+    }
+  }
+}
